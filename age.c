5cb600e2eb3ed1baf94e772c5083095bdf091f48
benjaaa-c <benjacl.sh4@gmail.com>
2025-04-01 14:45:42 +0000

    variables

---
+++ age.c
@@ -0,0 +1,19 @@
+/*This program takes age imput form the user
+It stores it in the age variable
+And, print the value using print()*/
+
+
+
+#include <stdio.h>
+
+
+int main() {
+    
+    int age = 18;//create interger variable
+
+    //print the age variable
+    printf("age: %d\n", age);
+
+    return 0;
+
+}
