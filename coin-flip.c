e90967b03f71eb027ccf0405659b62894f8d89ed
benjaaa-c <benjacl.sh4@gmail.com>
2025-05-14 14:52:17 +0000

    coin

---
+++ coin-flip.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+int guess 
+printf("heads or tails?\n1. heads\n2. tails\n");
+scanf(" %d", &guess)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
